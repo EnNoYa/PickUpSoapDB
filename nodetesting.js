c1820f28a966b11babf484c293526a1269cf75b1
ENY <36843641+EnNoYa@users.noreply.github.com>
2018-10-01 03:32:02 -0700

    insertfin

--- nodetesting.js
+++ nodetesting.js
@@ -3,34 +3,39 @@ var Connection = require('tedious').Connection;
         userName: 'SA',  
         password: '1qaz@WSX',  
         server: '127.0.0.1',  
-        // If you are on Azure SQL Database, you need these next options.  
+        // When you connect to Azure SQL Database, you need these next options.  
        // options: {encrypt: true, database: 'AdventureWorks'}  
     };  
     var connection = new Connection(config);  
     connection.on('connect', function(err) {  
         // If no error, then good to proceed.  
         console.log("Connected");  
-        executeStatement1();  
+        executeStatement();  
     });  
 
-    var Request = require('tedious').Request  
+    var Request = require('tedious').Request;  
     var TYPES = require('tedious').TYPES;  
 
-    function executeStatement1() {  
-        request = new Request("use TestDB; INSERT into ttest values(@id,@name) ;", function(err) {  
-         if (err) {  
+    function executeStatement() {  
+        request = new Request("use TestDB; SELECT * FROM ttest;", function(err) {  
+        if (err) {  
             console.log(err);}  
         });  
-        request.addParameter('id', TYPES.Int,5);  
-        request.addParameter('name', TYPES.NVarChar , 'S');  
+        var result = "";  
         request.on('row', function(columns) {  
             columns.forEach(function(column) {  
               if (column.value === null) {  
                 console.log('NULL');  
               } else {  
-                console.log("Product id of inserted item is " + column.value);  
+                result+= column.value + " ";  
               }  
             });  
-        });       
+            console.log(result);  
+            result ="";  
+        });  
+
+        request.on('done', function(rowCount, more) {  
+        console.log(rowCount + ' rows returned');  
+        });  
         connection.execSql(request);  
     }
