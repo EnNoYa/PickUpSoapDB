9a8e704d2a640518307b17bc91333150a2bbb809
ENY <36843641+EnNoYa@users.noreply.github.com>
2018-10-13 20:23:13 +0900

    fix

--- T&Test.js
+++ T&Test.js
@@ -1,8 +1,55 @@
 var express = require('express');
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-	//res.json({ user: 'testcontent' });
-  res.send('hello world');
+app.get('/', function (req, res) {
+
+    var Connection = require('tedious').Connection;  
+    var config = {  
+        userName: 'SA',  
+        password: '1qaz@WSX',  
+        server: '127.0.0.1',  
+       
+    };  
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        // If no error, then good to proceed.  
+        console.log("Connected");  
+        executeStatement();  
+    });  
+
+    var Request = require('tedious').Request;  
+    var TYPES = require('tedious').TYPES;  
+
+    function executeStatement() {  
+        request = new Request("use TestDB; SELECT *  FROM weather;", function(err) {  
+        if (err) {  
+            console.log(err);}  
+        });  
+        var result = "";  
+        request.on('row', function(columns) {  
+            columns.forEach(function(column) {  
+              if (column.value === null) {  
+                console.log('NULL');  
+              } else {  
+                result+= column.value + " ";  
+              }  
+            });  
+            res.send(result);  
+            result ="";  
+        });  
+
+        request.on('done', function(rowCount, more) {  
+        console.log(rowCount + ' rows returned');  
+        });  
+        connection.execSql(request);  
+    }  
+
+
+
+
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
