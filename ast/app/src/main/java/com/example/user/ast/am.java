2daba71470db39f3ff31c7ebf29eac5e6c3d3d7c
louisfghbvc <43661591+louisfghbvc@users.noreply.github.com>
2018-12-15 14:49:07 +0800

    20181215專案更新
    add map79標記
    +cardview 主畫面
    +設定那裡儲存資料
    備註:要安裝cardView在XML介面那裏

--- ast/app/src/main/java/com/example/user/ast/am.java
+++ ast/app/src/main/java/com/example/user/ast/am.java
@@ -28,8 +28,12 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.CircleOptions;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class am extends AppCompatActivity implements OnMapReadyCallback, LocationListener{
@@ -40,6 +44,9 @@ public class am extends AppCompatActivity implements OnMapReadyCallback, Locatio
     LatLng currPoint;
     boolean isGPSEnabled;      //GPS定位是否可用
     boolean isNetworkEnabled;  //網路定位是否可用
+    private int M = 79;//最大值
+    private Marker Marr[] = new Marker[M];
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -69,6 +76,8 @@ public class am extends AppCompatActivity implements OnMapReadyCallback, Locatio
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);//設定地圖類別
+        for(int i=0; i<M; ++i)
+            mMap.addMarker(new MarkerOptions().position(LLplace[i]).title(place_name[i]));
         mMap.setOnMyLocationButtonClickListener(onMyLocationButtonClickListener);//開啟按鈕事件
         mMap.setOnMyLocationClickListener(onMyLocationClickListener);//開啟點點事件
         checkPermission();//檢查權限
@@ -181,4 +190,107 @@ public class am extends AppCompatActivity implements OnMapReadyCallback, Locatio
                     mMap.addCircle(circleOptions);
                 }
             };
+    /*觀測站位置資訊*/
+    private LatLng LLplace[] = {
+                new LatLng(25.29743611,121.537975),
+                new LatLng(25.18272222,121.5295833),
+                new LatLng(25.17966667,121.6898806),
+                new LatLng(25.1645,121.4492389),
+                new LatLng(25.12916667,121.7600556),
+                new LatLng(25.10591667,121.5145),
+                new LatLng(25.07857,121.3657028),
+                new LatLng(25.07261111,121.4938056),
+                new LatLng(25.06895,121.4810278),
+                new LatLng(25.06566944,121.6408),
+                new LatLng(25.0632,	121.5133111),
+                new LatLng(25.06236111,121.5265278),
+                new LatLng(25.06034444,	121.2018111),
+                new LatLng(25.05,	121.5786111),
+                new LatLng(25.04650278,	121.5079722),
+                new LatLng(25.03797222,	121.4325),
+                new LatLng(25.03550278,	121.0827611),
+                new LatLng(25.02060833,	121.5295556),
+                new LatLng(25.017,	121.5163056),
+                new LatLng(25.01297222,	121.4586667),
+                new LatLng(24.98677778,	121.3087222),
+                new LatLng(24.98252778,	121.4518611),
+                new LatLng(24.97722222,	121.5377778),
+                new LatLng(24.95420833,	121.2049806),
+                new LatLng(24.95327778,	121.2216667),
+                new LatLng(24.86386944,	121.21635),
+                new LatLng(24.90014167,	121.0386528),
+                new LatLng(24.80561944,	120.972075),
+                new LatLng(24.69696944,	120.8985722),
+                new LatLng(24.56526944,	120.8202),
+                new LatLng(24.38294167,	120.7588333),
+                new LatLng(24.25658611,	120.7417111),
+                new LatLng(24.22562778,	120.5687944),
+                new LatLng(24.16219722,	120.6169167),
+                new LatLng(24.15195833,	120.6410917),
+                new LatLng(24.13167222,	120.4690611),
+                new LatLng(24.09961111,	120.6776889),
+                new LatLng(24.066,	120.5415194),
+                new LatLng(23.96884167,	120.9679028),
+                new LatLng(23.925175,	120.4096528),
+                new LatLng(23.913,	120.6853056),
+                new LatLng(23.75638889,	120.6773056),
+                new LatLng(23.75350556,	120.251825),
+                new LatLng(23.71753333,	120.2028417),
+                new LatLng(23.71185278,	120.5449944),
+                new LatLng(23.55483889,	120.3455306),
+                new LatLng(23.46530833,	120.24735),
+                new LatLng(23.46277778,	120.4408333),
+                new LatLng(23.30563333,	120.31725),
+                new LatLng(23.11509722,	120.2971417),
+                new LatLng(23.04916667,	120.2183333),
+                new LatLng(22.98458056,	120.2026167),
+                new LatLng(22.88358333,	120.5305417),
+                new LatLng(22.75750556,	120.3056889),
+                new LatLng(22.73366667,	120.3282889),
+                new LatLng(22.68905556,	120.3326306),
+                new LatLng(22.67486111,	120.2929167),
+                new LatLng(22.67308056,	120.4880333),
+                new LatLng(22.63256667,	120.2880861),
+                new LatLng(22.62739167,	120.3580833),
+                new LatLng(22.60871111,	120.3120167),
+                new LatLng(22.60538611,	120.3075639),
+                new LatLng(22.56583333,	120.3377361),
+                new LatLng(22.56413611,	120.425311),
+                new LatLng(22.52310833,	120.561175),
+                new LatLng(22.4795,	120.41175),
+                new LatLng(21.95806944,	120.7889278),
+                new LatLng(24.74791667,	121.7463944),
+                new LatLng(24.63220278,	121.7929278),
+                new LatLng(23.97130556,	121.5997694),
+                new LatLng(23.04508333,	121.1619333),
+                new LatLng(22.75535833,	121.15045),
+                new LatLng(26.15361111,	119.9525),
+                new LatLng(24.43213333,	118.3122556),
+                new LatLng(23.56903056,	119.5661583),
+                new LatLng(23.12216944,	120.4697361),
+                new LatLng(23.84315833,	120.2818139),
+                new LatLng(23.75754722,	120.3487417),
+                new LatLng(22.35222222,	120.3772222),
+        };
+        private String place_name[] = {
+                "富貴角", "陽明", "萬里", "淡水", "基隆", "士林", "林口", "三重", "菜寮", "汐止", "大同", "中山", "大園", "松山",
+                "萬華", "新莊", "觀音", "古亭", "永和", "板橋", "桃園", "土城", "新店", "平鎮", "中壢", "龍潭", "湖口", "新竹",
+                "頭份", "苗栗", "三義", "豐原", "沙鹿", "西屯", "忠明", "線西", "大里", "彰化", "埔里", "二林", "南投", "竹山",
+                "麥寮", "台西", "斗六", "新港", "圤子", "嘉義", "新營", "善化", "安南", "台南", "美濃", "橋頭", "楠梓", "仁武",
+                "左營", "屏東", "前金", "鳳山", "復興", "前鎮", "小港", "大寮", "潮州", "林園", "恆春", "宜蘭", "冬山", "花蓮",
+                "關山", "台東", "馬祖", "金門", "馬公", "台南(玉井)", "彰化(大城)", "崙背", "屏東(琉球)"
+        };
+        public int shortest_place(LatLng curL){
+            double dis = 1000.0;
+            int id=0;
+            for(int i=0; i<M; ++i){
+                double diff1=curL.latitude-LLplace[i].latitude,diff2=curL.longitude-LLplace[i].longitude;
+                if(diff1*diff1+diff2*diff2<dis){
+                    id=i;
+                    dis=diff1*diff1+diff2*diff2;
+                }
+            }
+            return id;
+        }
+
 }
--- ast/app/src/main/java/com/example/user/ast/ns.java
+++ ast/app/src/main/java/com/example/user/ast/ns.java
@@ -1,6 +1,7 @@
 package com.example.user.ast;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,9 @@ public class ns extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_ns);
+
+    SharedPreferences SettingsSave = getSharedPreferences("settingsave",MODE_PRIVATE);
+    final SharedPreferences.Editor editorsettings = SettingsSave.edit();
 
     intent = new Intent();
 
@@ -51,51 +55,143 @@ public class ns extends AppCompatActivity {
             android.R.layout.simple_spinner_dropdown_item);
     sp.setAdapter(sprna);
 
-    Switch ics = (Switch)findViewById(R.id.inss);
+    final Switch ics = (Switch)findViewById(R.id.inss);
     final Switch icv = (Switch)findViewById(R.id.insv);
     final Switch icr = (Switch)findViewById(R.id.irts);
+    Switch rcs = (Switch)findViewById(R.id.rnss);
+    final Switch rcv = (Switch)findViewById(R.id.rnsv);
+    final Switch rcr = (Switch)findViewById(R.id.rrts);
+    final Spinner rsp = (Spinner)findViewById(R.id.rnssp);
     ins.setEnabled(false);
     icv.setEnabled(false);
     icr.setEnabled(false);
     ins.setEnabled(false);
+    rns.setEnabled(false);
+    rcv.setEnabled(false);
+    rcr.setEnabled(false);
+    rns.setEnabled(false);
+    rsp.setEnabled(false);
+
+    if(SettingsSave.getBoolean("icschecked",true)==true){
+      ics.setChecked(true);
+      icv.setEnabled(true);
+      icr.setEnabled(true);
+    }
+    else{
+      ics.setChecked(false);
+      icv.setEnabled(false);
+      icr.setEnabled(false);
+      ins.setEnabled(false);
+    }
+
+    if(SettingsSave.getBoolean("icvchecked",true)==true){
+      icv.setChecked(true);
+    }
+    else{
+      icv.setChecked(false);
+    }
+
+    if(SettingsSave.getBoolean("icrchecked",true)==true){
+      icr.setChecked(true);
+      if(ics.isChecked())
+        ins.setEnabled(true);
+    }
+    else{
+      icr.setChecked(false);
+      ins.setEnabled(false);
+    }
+
+    if(SettingsSave.getBoolean("rcschecked",true)==true){
+      rcs.setChecked(true);
+      rcv.setEnabled(true);
+      rcr.setEnabled(true);
+      rsp.setEnabled(true);
+    }
+    else{
+      rcs.setChecked(false);
+      rcv.setEnabled(false);
+      rcr.setEnabled(false);
+      rns.setEnabled(false);
+      rsp.setEnabled(false);
+    }
+
+    if(SettingsSave.getBoolean("rcvchecked",true)==true){
+      rcv.setChecked(true);
+    }
+    else{
+      rcv.setChecked(false);
+    }
+
+    if(SettingsSave.getBoolean("rcrchecked",true)==true){
+      rcr.setChecked(true);
+      if(rcs.isChecked())
+        rns.setEnabled(true);
+    }
+    else{
+      rcr.setChecked(false);
+      rns.setEnabled(false);
+    }
+
+
+
     ics.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
       public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (isChecked) {
+        if (ics.isChecked()) {
+          editorsettings.putBoolean("icschecked",true);
+          editorsettings.apply();
           icv.setEnabled(true);
           icr.setEnabled(true);
+          if(icr.isChecked())
+            ins.setEnabled(true);
         } else {
+          editorsettings.putBoolean("icschecked",false);
+          editorsettings.apply();
           icv.setEnabled(false);
           icr.setEnabled(false);
           ins.setEnabled(false);
         }
       }
     });
+
+    icv.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (icv.isChecked()) {
+          editorsettings.putBoolean("icvchecked",true);
+          editorsettings.apply();
+        } else {
+          editorsettings.putBoolean("icvchecked",false);
+          editorsettings.apply();
+        }
+      }
+    });
+
     icr.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
       public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if (isChecked) {
+          editorsettings.putBoolean("icrchecked",true);
+          editorsettings.apply();
           ins.setEnabled(true);
         } else {
+          editorsettings.putBoolean("icrchecked",false);
+          editorsettings.apply();
           ins.setEnabled(false);
         }
       }
     });
 
-    Switch rcs = (Switch)findViewById(R.id.rnss);
-    final Switch rcv = (Switch)findViewById(R.id.rnsv);
-    final Switch rcr = (Switch)findViewById(R.id.rrts);
-    final Spinner rsp = (Spinner)findViewById(R.id.rnssp);
-    rns.setEnabled(false);
-    rcv.setEnabled(false);
-    rcr.setEnabled(false);
-    rns.setEnabled(false);
-    rsp.setEnabled(false);
     rcs.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
       public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if (isChecked) {
+          editorsettings.putBoolean("rcschecked",true);
+          editorsettings.apply();
           rcv.setEnabled(true);
           rcr.setEnabled(true);
           rsp.setEnabled(true);
+          if(rcr.isChecked())
+            rns.setEnabled(true);
         } else{
+          editorsettings.putBoolean("rcschecked",false);
+          editorsettings.apply();
           rcv.setEnabled(false);
           rcr.setEnabled(false);
           rns.setEnabled(false);
@@ -103,11 +199,28 @@ public class ns extends AppCompatActivity {
         }
       }
     });
+
+    rcv.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (rcv.isChecked()) {
+          editorsettings.putBoolean("rcvchecked",true);
+          editorsettings.apply();
+        } else {
+          editorsettings.putBoolean("rcvchecked",false);
+          editorsettings.apply();
+        }
+      }
+    });
+
     rcr.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
       public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if (isChecked) {
+          editorsettings.putBoolean("rcrchecked",true);
+          editorsettings.apply();
           rns.setEnabled(true);
         } else {
+          editorsettings.putBoolean("rcrchecked",false);
+          editorsettings.apply();
           rns.setEnabled(false);
         }
       }
