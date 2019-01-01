1d0b6858521c0acf87b214e05218ddee7ebadf4b
louisfghbvc <43661591+louisfghbvc@users.noreply.github.com>
2019-01-01 11:05:53 +0800

    更新UI

--- 更新檔案請直接丟入專案app_src_main取代/main/java/com/example/user/ast/MainActivity.java
+++ 更新檔案請直接丟入專案app_src_main取代/main/java/com/example/user/ast/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.user.ast;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.constraint.Guideline;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -55,6 +60,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Guideline x = findViewById(R.id.bsl);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        float scrheight = dm.ydpi/160*dm.heightPixels;  //螢幕的高
+        //dip/px=dpi/160
+        //dip=dpi/160*px
+
+        x.setGuidelineEnd((int)(dm.heightPixels*0.15));
 
     }
 }
